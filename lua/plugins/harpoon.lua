ad34d8a484e763d801f66ce7d35ed99a4fff0fae
Mihkel Väli <mvali@consynsys.com>
2024-04-08 07:57:05 +0300

    add harpoon, commentary, new trouble version and undotree

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,6 +6,8 @@ return {
   {
     "ThePrimeagen/harpoon",
     name = "harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("harpoon").setup()
     end,
