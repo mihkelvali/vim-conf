ab63a1542940b14564dfef1dca29cebfdf745a2c
Mihkel Väli <mvali@consynsys.com>
2025-07-04 14:41:35 +0300

    feat: remove copilot

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 --- lua
-vim.g.copilot_node_command = "~/.nvm/versions/node/v20.19.0/bin/node"
